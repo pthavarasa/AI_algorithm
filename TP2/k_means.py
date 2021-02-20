7dce6267103d7161152e8e3fe98ddb961b48ffed
Thavarasa Prasanth <45243326+pthavarasa@users.noreply.github.com>
2021-02-20 15:50:47 +0100

    KMEANS class is ready to use - removed matplotlib debug mode - removed random generated data - added comments for class

--- TP2/k_means.py
+++ TP2/k_means.py
@@ -1,17 +1,26 @@
 from random import randint
 from random import choice
 from random import sample
-import numpy as np
-import matplotlib.pyplot as plt
 
 
 class KMEANS:
+  """KMEANS class :)
+  Attribute : 
+  - k : Clusters lenght
+  - data : Multidimensional data array
+  Methods :
+  - distance : Euclidean distance of 2 array
+  - assign_cluster : Assign objects to their closest cluster center according to the Euclidean distance
+  - centroid : Calculate the centroid or mean of all objects in each cluster
+  - inertie_intra_classe : Total intra-cluster variance
+  - learn : Repeat func assign_cluster and centroid until the same points are assigned to each cluster in consecutive rounds
+  Note : Unoptimized code - https://code.up8.edu/pthavarasa/ai-tp/-/commit/a26271ce7ba173ccbc729ef7e10769cdada9d588
+  """
   def __init__(self, k, data):
     self.k = k
     self.data = data
     self.clusters = [[] for _ in range(self.k)]
     self.centers = sample(self.data, self.k)
-    self.color = ["#96%04x" % randint(0, 0xFFFF) for _ in range(self.k)]
   
   def distance(self, p1, p2):
     return sum(abs(p1[i] - p2[i]) for i in range(len(p1)))
@@ -23,54 +32,13 @@ class KMEANS:
 
   def centroid(self):
     self.centers = [[sum(d) * (1/len(c)) for d in zip(*c)] for c in self.clusters]
-  
-  def show_cluster(self):
-    for i in range(len(self.clusters)):
-      plt.scatter([point[0] for point in self.clusters[i]], [point[1] for point in self.clusters[i]], color=self.color[i])
-  
-  def show_centers(self):
-    for c in self.centers:
-      plt.scatter(c[0], c[1], color="red")
 
   def inertie_intra_classe(self):
     return sum(pow(self.distance(i, self.centers[c]), 2) for c in range(self.k) for i in self.clusters[c])
   
-  def run(self):
-    condition = True
-    while condition:
+  def learn(self):
+    while True:
       self.assign_cluster()
       inertie = self.inertie_intra_classe()
-      self.show_cluster()
-      self.show_centers()
-      plt.show()
       self.centroid()
-      condition = inertie > self.inertie_intra_classe()
-
-
-
-
-centre1=np.array([3,3])
-centre2=np.array([-3,-3])
-sigma1=np.array([[1.5,0],[0,1.5]])
-sigma2=np.array([[1.5,0],[0,1.5]])
-taille1=200
-taille2=200
-cluster1=np.random.multivariate_normal(centre1,sigma1,taille1)
-cluster2=np.random.multivariate_normal(centre2,sigma2,taille2)
-
-data = []
-for i in cluster1:
-	data.append([i[0], i[1]])
-for i in cluster2:
-	data.append([i[0], i[1]])
-
-KM = KMEANS(2, data)
-KM.run()
-
-#plt.scatter([point[0] for point in cluster1], [point[1] for point in cluster1], color="pink")
-#plt.scatter([point[0] for point in cluster2], [point[1] for point in cluster2], color="blue")
-#plt.scatter(centre1[0], centre1[1], color="red")
-#plt.scatter(centre2[0], centre2[1], color="red")
-
-#plt.show()
-
+      if not inertie > self.inertie_intra_classe(): break
